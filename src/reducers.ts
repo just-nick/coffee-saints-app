838fc808fdcc40e5ae05703299263940a155e5ea
Moto Me <support@motome.com.au>
2017-07-07 17:27:11 +1000

    Yolo typing

--- src/reducers.ts
+++ src/reducers.ts
@@ -3,8 +3,8 @@ import {saintReducer} from './app/saint/saint.reducer';
 import {buyerReducer} from "./app/buyer/buyer.reducer";
 
 export let reducers: CoffeeSaintsReducers = {
-    saintReducer,
-    buyerReducer
+    saintReducer: saintReducer as any,
+    buyerReducer: buyerReducer as any
 };
 
 export interface CoffeeSaintsReducers extends ReducersMapObject {
